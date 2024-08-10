896a124838523890b58e367ed79bf279bba3ce37
riannalei <riannalei@gmail.com>
2024-08-10 14:43:47 -0700

    sessions and cookies

--- app.js
+++ app.js
@@ -3,23 +3,44 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://riannalei:1527168Rxl%3B@cluster0.rwpe4.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: '223388',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('66b6ba724dc2df4091f9362f')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -29,19 +50,18 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://riannalei:1527168Rxl%3B@cluster0.rwpe4.mongodb.net/shop?retryWrites=true&w=majority'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
+          name: 'Rianna',
+          email: 'rianna@test.com',
           cart: {
             items: []
           }
@@ -49,10 +69,10 @@ mongoose
         user.save();
       }
     });
-    app.listen(3000);
+    app.listen(3000, () => {
+      console.log('Server is running on http://localhost:3000');
+    });
   })
   .catch(err => {
     console.log(err);
   });
-
-  
